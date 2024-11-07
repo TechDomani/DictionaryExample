3964fdc286d608696023fa81d6d8d2f905d85acf
TechDomani <73493660+TechDomani@users.noreply.github.com>
2024-11-07 13:59:53 +0000

    Initial commit

---
+++ DictionaryExample/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace DictionaryExample
+{
+	internal class Program
+	{
+		static void Main()
+		{
+			Frequency();			
+		}
+
+		private static void StudentDictionary()
+		{
+			Student bob = new("Bob", "Marley", new DateOnly(1950, 11, 10));
+			bob.AddSubject("French");
+			bob.AddSubject("Music");
+			Console.WriteLine(bob.GetSubjects());
+
+			Student bob2 = new("Bob", "Dylan", new DateOnly(1970, 10, 09));
+			Student susie = new("Susie", "Smith", new DateOnly(1980, 03, 8));
+
+			Dictionary<string, Student> studentDict = [];
+			studentDict.Add(bob.FullName, bob);
+			studentDict.Add(bob2.FullName, bob2);
+			studentDict.Add(susie.FullName, susie);
+
+			Console.WriteLine(studentDict["Bob Marley"].GetSubjects());
+		}
+
+		private static void Frequency()
+		{
+			List<string> pets = ["cat", "dog", "budgie", "rabbit", "fox", "dog", "cat", "cat", "cheetah", "rabbit", "dog"];
+			Dictionary<string, int> petFrequency = [];
+			foreach (string pet in pets)
+			{
+				if (!petFrequency.TryAdd(pet, 1))
+				{
+					petFrequency[pet] += 1;
+				}
+			}
+			Console.WriteLine($"There are {petFrequency["dog"]} dogs");
+		}
+	}
+}
---
+++ DictionaryExample/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryExample
+{
+	internal class Student
+	{
+		// Fields
+		private string _firstName;
+		private string _surname;
+		private List<string> _subjects = [];
+
+		// Properties
+		public string FullName
+		{
+			get
+			{
+				return $"{_firstName} {_surname}";
+			}
+		}
+		public DateOnly DateOfBirth { get; private set; }
+
+		//Constructor
+		public Student(string firstName, string surname, DateOnly dateOfBirth)
+		{
+			_firstName = firstName;
+			_surname = surname;
+			DateOfBirth = dateOfBirth;
+		}
+
+		// Methods
+		public void AddSubject(string subject)
+		{
+			if (!_subjects.Contains(subject))
+			{
+				_subjects.Add(subject);
+			}
+		}
+
+		public string GetSubjects()
+		{
+			return string.Join("\n", _subjects);
+		}
+
+	}
+}
